3e9f61461cd3d7eeb8daf6d3413bca926d2c2b57
jwi <jwi2018@naver.com>
2023-06-09 09:33:29 +0900

    코드 확인용 프로젝트 이동

---
+++ Assets/FlurryManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using FlurrySDK;
+
+public class FlurryManager : Singleton<FlurryManager>
+{
+#if UNITY_ANDROID
+    private string FLURRY_API_KEY = "SF42FM2JQJ6Q2RRVS3FD";
+#elif UNITY_IPHONE
+    private string FLURRY_API_KEY = "4TD6Y4DGKD5DQXNZX7CQ";
+#else
+    private string FLURRY_API_KEY = null;
+#endif
+
+    public void Init()
+    {
+        // Initialize Flurry.
+        new Flurry.Builder()
+                  .WithCrashReporting(true)
+                  .WithLogEnabled(true)
+                  .WithLogLevel(Flurry.LogLevel.VERBOSE)
+                  .WithMessaging(true)
+                  .Build(FLURRY_API_KEY);
+    }
+}
---
+++ Assets/Shining Coins/ShiningItem.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShiningItem : MonoBehaviour {
+
+	public float shiningTime = 1f;
+	public float width = 0.2f;
+
+    GraphicRaycaster m_Raycaster;
+    PointerEventData m_PointerEventDataㄹㄹ;
+    EventSystem m_EventSystem;
+    Image image;
+    bool isShining = false;
+	SpriteRenderer sr;
+
+
+	// Use this for initialization
+	void Start () {
+        if(this.GetComponent<SpriteRenderer>() != null)
+        {
+            sr = GetComponent<SpriteRenderer>();
+        }
+        if(this.GetComponent<Image>() != null)
+        {
+            image = GetComponent<Image>();
+        }
+
+        m_Raycaster = GetComponent<GraphicRaycaster>();
+        m_EventSystem = GetComponent<EventSystem>();
+	}
+
+    public void StartShine()
+    {
+        StartCoroutine(Shine());
+    }
+
+	IEnumerator Shine () {
+		float currentTime = 0;
+		float speed = 1f / shiningTime;
+
+        if(sr != null)
+        {
+            sr.material.SetFloat("_Width", width);
+        }
+        if(image != null)
+        {
+            image.material.SetFloat("_Width", width);
+        }
+		while (currentTime <= shiningTime) {
+			currentTime += Time.deltaTime;
+			float value = Mathf.Lerp (0, 1, speed * currentTime);
+            if(sr != null)
+            {
+                sr.material.SetFloat("_TimeController", value);
+            }
+            if(image != null)
+            {
+                image.material.SetFloat("_TimeController", value);
+            }
+			yield return null;
+		}
+		yield return new WaitForSeconds (1f);
+        if(sr != null)
+        {
+            sr.material.SetFloat("_Width", 0);
+        }
+        if(image != null)
+        {
+            image.material.SetFloat("_Width", 0);
+        }
+		isShining = false;
+	}
+
+    private void OnDestroy()
+    {
+        if(sr != null)
+        {
+            sr.material.SetFloat("Width", 0.2f);
+            sr.material.SetFloat("_TimeController", 0);
+        }
+        if(image != null)
+        {
+            image.material.SetFloat("Width", 0.2f);
+            image.material.SetFloat("_TimeController", 0);
+        }
+
+    }
+}
